97f4d87174a1593819aba1418be662ada705802e
witchbaik <daffafariz31@gmail.com>
2025-04-29 00:22:45 +0700

    Selesaikan tugas landing page

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,10 @@ use Illuminate\Support\Facades\Route;
 
 // Route::permanentRedirect('/', '/login');
 
+Route::get('/', function () {
+    return view('landing-page');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
