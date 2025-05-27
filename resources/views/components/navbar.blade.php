0ccca7cbbfe8c916e095ea870a07860296e4797c
FaizIsmail <faizismail1706@gmail.com>
2025-05-27 14:57:54 +0700

    responsive index jadwal pengambilan

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -12,6 +12,12 @@
 
     <!-- Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <!-- Essential Meta Tags untuk Responsive -->
+    <meta charset="utf-8">
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no, maximum-scale=1">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="msapplication-tap-highlight" content="no">
 
     <!-- Additional styles -->
     @yield('styles')
