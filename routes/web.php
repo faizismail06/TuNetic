356a0d82c82c08947edd40808b4661d1d547e3da
MilaDewii <milanrhd13@gmail.com>
2025-06-12 22:56:01 +0700

    update rute kelola petugas

--- routes/web.php
+++ routes/web.php
@@ -100,7 +100,7 @@ Route::resource('pusat/manage-permission', PermissionController::class)->only('s
 // Route::get('petugas/{id}/detail', [PetugasController::class, 'showDetail'])->name('petugas.detail');
 // Route::get('petugas/create', [PetugasController::class, 'create'])->name('petugas.create');
 // Route::post('petugas', [PetugasController::class, 'destroy'])->name('petugas.destroy');
-// Route::post('petugas', [PetugasController::class, 'index'])->name('petugas.index');
+Route::post('petugas', [PetugasController::class, 'index'])->name('petugas.index');
 Route::get('/rute/{id}/detail', [RuteController::class, 'show'])->name('rute.detail');
 Route::get('/rute/{id_rute}/detail', [RuteController::class, 'detail'])->name('rute.detail');
 Route::get('/api/rute/{id}', [RuteController::class, 'getDetailJson'])->name('api.rute.detail');
